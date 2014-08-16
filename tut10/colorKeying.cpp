d356b3bb074fdcf5124010a9264a22d0e36ae72d
wedemalm <manfred.wedemalm@gmail.com>
2014-08-16 10:33:21 +0200

    tut10 done

---
+++ tut10/colorKeying.cpp
@@ -0,0 +1,231 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH  640
+
+//The window we'll be rendering to
+SDL_Window *g_window = NULL;
+
+//The window renderer
+SDL_Renderer *g_renderer = NULL;
+
+//Texture wrapper class
+class LTexture {
+public:
+  //Initializes variables
+  LTexture();
+
+  //Deakkicates memory
+  ~LTexture();
+
+  //Loads image at specified path
+  bool loadFromFile(std::string path);
+
+  //Deallocates texture
+  void free();
+
+  //Renders Texture at given point
+  void render(int x, int y);
+
+  //Gets image dimensions
+  int getWidth();
+  int getHeight();
+
+private:
+  //The actual hardware texture
+  SDL_Texture *m_texture;
+
+  //Image dimensions
+  int m_width;
+  int m_height;
+};
+
+LTexture::LTexture() {
+  //Initialize
+  m_texture = NULL;
+  m_width   = 0;
+  m_height  = 0;
+}
+
+LTexture::~LTexture() {
+  //Deallocate
+  free();
+}
+
+bool LTexture::loadFromFile(std::string path) {
+  //Get rid of preexisting texture
+  free();
+
+  //The final texture
+  SDL_Texture *newTexture = NULL;
+  
+  //Load image at specified path
+  SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+  if(loadedSurface == NULL) {
+    printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+  } else {
+    //Color key image
+    SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+    //Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface(g_renderer, loadedSurface);
+    if(newTexture == NULL) {
+      printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    } else {
+      //Get image dimensions
+      m_width  = loadedSurface->w;
+      m_height = loadedSurface->h;
+    }
+    //Get rid of ol loaded surface
+    SDL_FreeSurface(loadedSurface);
+  }
+  //Return success
+  m_texture = newTexture;
+  return m_texture != NULL;
+}
+
+void LTexture::free() {
+  //Free texture if it exists
+  if(m_texture != NULL) {
+    SDL_DestroyTexture(m_texture);
+    m_texture = NULL;
+    m_width   = 0;
+    m_height  = 0;
+  }
+}
+
+void LTexture::render(int x, int y) {
+  //Set rendering space and render to screen
+  SDL_Rect renderQuad = {x, y, m_width, m_height};
+  SDL_RenderCopy(g_renderer, m_texture, NULL, &renderQuad);
+}
+
+int LTexture::getWidth() {
+  return m_width;
+}
+
+int LTexture::getHeight() {
+  return m_height;
+}
+
+//Scene textures
+LTexture g_fooTexture;
+LTexture g_backgroundTexture;
+
+bool loadMedia() {
+  //Loading success flag
+  bool success = true;
+
+  //Load foo' texture
+  if(!g_fooTexture.loadFromFile("./foo.png")) {
+    printf("Failed to load foo' texture image!\n");
+    success = false;
+  }
+
+  //Load background texture
+  if(!g_backgroundTexture.loadFromFile("./background.png")) {
+    printf("Failed to load background texture image!\n");
+    success = false;
+  }
+  return success;
+}
+
+bool init() {
+  bool l_success = true;
+
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize! SDL Error: %s\n",
+	   SDL_GetError());
+    l_success = false;
+  } else {
+    if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+      printf("Warning: Linear texture filtering not enabled");
+    }  
+    //Create window
+    g_window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, 
+				SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+				SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  
+    if(g_window == NULL) {
+      printf("Window could not be created! SDL Error: %s\n",
+	     SDL_GetError());
+      l_success = false;
+    } else {
+      //Creates renderer for window
+      g_renderer = SDL_CreateRenderer(g_window, -1,
+				      SDL_RENDERER_ACCELERATED);
+      if(g_renderer == NULL) {
+	printf("Renderer could not be created! SDL Error:%s\n",
+	       SDL_GetError());
+	l_success = false;
+      } else {
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	int imgFlags = IMG_INIT_PNG;
+	if(!(IMG_Init(imgFlags) & imgFlags)) {
+	  printf("SDL_image could not initialize! SDL_image Error: %s\n",
+		 IMG_GetError());
+	  l_success = false;
+	}
+      }
+    }
+  }
+  return l_success;
+}
+
+void close() {
+  //Free loaded images
+  g_fooTexture.free();
+  g_backgroundTexture.free();
+
+  //Destroy window
+  SDL_DestroyRenderer(g_renderer);
+  SDL_DestroyWindow(g_window);
+  g_window   = NULL;
+  g_renderer = NULL;
+
+  //Quit SDL subsystem
+  IMG_Quit();
+  SDL_Quit();
+}
+
+int main() {
+  bool quit = false;
+  SDL_Event e;
+
+  if(!init()) {
+    printf("Failed to initialize!\n");
+  }
+
+  if(!loadMedia()) {
+    printf("Failed to load media!\n");
+    return -1;
+  }
+
+  //While application is running
+  while(!quit) {
+    //Handle events on queue
+    while(SDL_PollEvent(&e) != 0) {
+      //User request quit
+      if(e.type == SDL_QUIT) {
+	quit = true;
+      }
+    }
+    //Clear screen
+    SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(g_renderer);
+
+    //Render background texture to screen
+    g_backgroundTexture.render(0, 0);
+
+    //Render Foo' to the screen
+    g_fooTexture.render(240, 190);
+
+    //Update screen
+    SDL_RenderPresent(g_renderer);
+  }
+  close();
+  return 0;
+}
